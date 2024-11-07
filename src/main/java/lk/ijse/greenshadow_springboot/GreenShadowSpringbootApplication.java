668772d9df1563e7cee63a580eb3abd5a8988919
thisangaThewanmi <thisangat2005@gmail.com>
2024-11-07 22:04:30 +0530

    Resolved the error

--- src/main/java/lk/ijse/greenshadow_springboot/GreenShadowSpringbootApplication.java
+++ src/main/java/lk/ijse/greenshadow_springboot/GreenShadowSpringbootApplication.java
@@ -1,5 +1,6 @@
 package lk.ijse.greenshadow_springboot;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/test/java/lk/ijse/greenshadow_springboot/GreenShadowSpringbootApplicationTests.java
+++ src/test/java/lk/ijse/greenshadow_springboot/GreenShadowSpringbootApplicationTests.java
@@ -3,6 +3,7 @@ package lk.ijse.greenshadow_springboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class GreenShadowSpringbootApplicationTests {
 
