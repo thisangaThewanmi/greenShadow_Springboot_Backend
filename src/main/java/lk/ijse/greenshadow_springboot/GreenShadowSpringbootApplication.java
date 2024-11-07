55ab5bd34b3902e27fa42b6feaa20e9d97ede697
thisangaThewanmi <thisangat2005@gmail.com>
2024-11-07 21:58:52 +0530

    initial commit

---
+++ src/main/java/lk/ijse/greenshadow_springboot/GreenShadowSpringbootApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.greenshadow_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenShadowSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenShadowSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/greenshadow_springboot/GreenShadowSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.greenshadow_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenShadowSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
