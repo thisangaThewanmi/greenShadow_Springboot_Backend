2b671a00be64651dd6d213bfb59b7250ba0f1c29
thisangaThewanmi <thisangat2005@gmail.com>
2024-12-02 01:07:15 +0530

    added a body to response entity in staffController

--- src/main/java/lk/ijse/greenshadow_springboot/controller/StaffController.java
+++ src/main/java/lk/ijse/greenshadow_springboot/controller/StaffController.java
@@ -17,6 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -30,7 +31,7 @@ public class StaffController  {
 
     @CrossOrigin(origins = "http://localhost:63342")
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> addStaff(@RequestBody StaffDto staffDto) {
+    public ResponseEntity<HashMap> addStaff(@RequestBody StaffDto staffDto) {
         System.out.println("staffDto comes to: " + staffDto);
 
         try {
@@ -40,16 +41,23 @@ public class StaffController  {
 
 
             //if sucessfully saved
-            return new ResponseEntity<>(HttpStatus.CREATED);
+           /* return new ResponseEntity<>(HttpStatus.CREATED);*/
+
+            return new ResponseEntity<>(new HashMap<String, String>() {{
+                put("message", "Staff added successfully");}}, HttpStatus.CREATED);
+
 
             //catching the error which comes from service layer
         } catch (DataPersistException e) {
             e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new HashMap<String, String>() {{
+                put("message", "Error occured while saving");}}, HttpStatus.BAD_REQUEST);
+
 
             //if there is an error which dosent occour from service
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new HashMap<String, String>() {{
+                put("message", "Change the email");}}, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -90,12 +98,16 @@ public class StaffController  {
             }
 
             staffService.deleteStaff(staffId);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(new HashMap<String, String>() {{
+                put("message", "Staff deleted successfully");}}, HttpStatus.NO_CONTENT);
+
 
         } catch (StaffNotFoundException e) {
             e.printStackTrace();
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            } catch (Exception e) {
+            return new ResponseEntity<>(new HashMap<String, String>() {{
+                put("message", "Staff id was not found");}}, HttpStatus.NOT_FOUND);
+
+        } catch (Exception e) {
             e.printStackTrace();
                 return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
@@ -105,7 +117,7 @@ public class StaffController  {
 
          @CrossOrigin(origins = "http://localhost:63342")
         @PutMapping(value="/{staffId}")
-        public ResponseEntity<Void> updateStaff(@PathVariable("staffId") String staffId,@RequestBody StaffDto staffDto) {
+        public ResponseEntity<HashMap> updateStaff(@PathVariable("staffId") String staffId,@RequestBody StaffDto staffDto) {
 
             System.out.println(staffDto);
             try {
@@ -114,10 +126,14 @@ public class StaffController  {
                     return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
                 }
                 staffService.updateStaff(staffId, staffDto);
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+                return new ResponseEntity<>(new HashMap<String, String>() {{
+                    put("message", "Staff updated Sucessfully");}}, HttpStatus.NO_CONTENT);
+
             }catch (StaffNotFoundException e) {
                 e.printStackTrace();
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<>(new HashMap<String, String>() {{
+                    put("message", "Staff was not found");}}, HttpStatus.BAD_REQUEST);
+
             }catch (Exception e) {
                 e.printStackTrace();
                 return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
